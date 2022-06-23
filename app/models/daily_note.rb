642c2fd08f0042022e9e978f7b581436b6aa3c13
JedwardMook <jedwardmook@yahoo.com>
2022-06-23 17:37:20 -0500

    Adds another seed with a create time and a method to grab day of the week

--- app/models/daily_note.rb
+++ app/models/daily_note.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 class DailyNote < ActiveRecord::Base
     belongs_to :employee
+
+    def day_of_the_week
+        day = self.created_on.wday
+            if day == 0
+                puts "Sunday"
+            elsif day == 1
+                puts "Monday"
+            elsif day == 2
+                puts "Tuesday"
+            elsif day == 3
+                puts "Wednesday"
+            elsif day == 4
+                puts "Thursday"
+            elsif day == 5
+                puts "Friday"
+            elsif day == 6
+                puts "Saturday"
+            else
+        end
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ puts "🌱 Seeding spices..."
 e1 = Employee.create(first_name: "Marena", last_name: "Smith", email_address: "m.smith@gamil.com")
 
 DailyNote.create(coffee_notes: "4 pots sold. Mexico on espresso", needs: "Tip money", items_86ed: "simple syrup", pastry_soldout: false, leftover_pastry: "2 cinnamon rolls", miscellaneous: "N/A", created_on: Time.new, employee_id: e1.id )
+DailyNote.create(coffee_notes: "5 pots sold. Ethiopia on drip", needs: "Nothing", items_86ed: "Stocked", pastry_soldout: true, leftover_pastry: "N/A", miscellaneous: "Good day for pastry", created_on: Time.new(2022, 6, 22, 2, 3, 2, "-05:00"), employee_id: e1.id )
 
 
 
