d83b0e564032beb835e3e1d2c5608833ac0a7434
JedwardMook <jedwardmook@yahoo.com>
2022-07-05 14:58:29 -0500

    Removes original route, adds a post for /employees, a get for /employees, a get for /daily_notes and /daily_notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,29 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Thanks!" }.to_json
-  end
 
-  get "/employee" do
+  get "/employees" do
     employees = Employee.all
     employees.to_json
+  end
+
+  post "/employees" do
+    employee = Employee.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email_address: params[:email_address]
+    )
+    employee.to_json
+  end
+
+  get "/daily_notes" do
+    daily_notes = DailyNote.all.order(:created_on)
+    daily_notes.to_json(include: :employee)
+  end
+
+  get '/daily_notes/:id' do
+    daily_note = DailyNote.find(params[:id])
+    daily_note.to_json
   end
 
 end
